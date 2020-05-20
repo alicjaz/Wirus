2d89fc8ff5e371357339498590505459851ead49
Alicja Zalewska <alza408@gmail.com>
2020-05-20 16:09:12 +0200

    Poprawione style

--- src/views/searchMovie/SearchMovie.tsx
+++ src/views/searchMovie/SearchMovie.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import NavPanel from '../../components/navPanel/NavPanel'
 import movieService, { IMoviesProps } from '../../services/movies.service';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardHeader, CardContent, CardMedia, Divider, Typography} from '@material-ui/core';
+import { Card, CardContent, CardMedia, Typography, CardActionArea} from '@material-ui/core';
+import { DebounceInput } from 'react-debounce-input';
 
 const useStyles = makeStyles ({
   card: {
@@ -18,6 +19,13 @@ const useStyles = makeStyles ({
     maxWidth: 350,
     margin: "auto",
     transition: "0.3s",
+    border: "2px solid",
+    boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+    "&:hover": {
+      boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+    }
+  },
+   title: {
     border: "2px solid",
     boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
     "&:hover": {
@@ -47,38 +55,38 @@ const SearchMovie = () => {
 
 
   return (
-    <div>
-      <NavPanel/>
-      <input
-        placeholder="Enter movie name"
-        onChange={event => setMovieToSearch(event.target.value)}
-      />
-      <Card>
-        <Card className={classes.card}>
-          <CardContent className={classes.content}>
-            {!!movies?.movies.length &&
-            movies?.movies.map(movie => (
-              <Card key={movie.id}>
-                <Card className={classes.content}>
-                  <img src={movie.poster}
-                       alt={movie.title}/>
-                  <br />
-                  {movie.title}
-                  <br />
-                   {movie.type}
-                  <br />
-                   {movie.year}
-                  </Card>
-              </Card>
-            ))
-            }
-          </CardContent>
-        </Card>
-       
-      </Card>
-      
-    </div>
+      <div >
+        <NavPanel />
+        <DebounceInput
+            debounceTimeout={500}
+            placeholder="Enter movie name"
+            onChange={event => setMovieToSearch(event.target.value)}
+            style={{ all:"initial",fontFamily:"Arial",width: "200px",margin:"0 auto",display:"block",backgroundColor:"transparent",border:"none",borderBottom: "1px solid blue",textAlign:"center",height:"30px",marginBottom:"20px",marginTop:"20px"}}
+        />
+
+        {!!movies?.movies.length &&
+        movies?.movies.map(movie => (
+            <Card style={{margin:"auto",marginBottom:"20px",width:"300px"}} className={classes.content}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {movie.title}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+
+                    {movie.year}
+                  </Typography>
+                </CardContent>
+                <CardMedia>
+                  <img src={movie.poster}></img>
+                </CardMedia>
+
+
+              </CardActionArea>
+            </Card>
+        ))
+        }
+      </div>
   );
 };
-
 export default SearchMovie;
